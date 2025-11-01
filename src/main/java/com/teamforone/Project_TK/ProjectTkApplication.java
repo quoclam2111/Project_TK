a938c31ef975532b8549653c0f4d031b429a1a26
Nguyễn Vũ Khoa <150508398+Nvkhoaaa@users.noreply.github.com>
2025-11-01 22:36:39 +0700

    NvkhoaDev Commit first

---
+++ src/main/java/com/teamforone/Project_TK/ProjectTkApplication.java
@@ -0,0 +1,13 @@
+package com.teamforone.Project_TK;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectTkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectTkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/teamforone/Project_TK/ProjectTkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamforone.Project_TK;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectTkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
